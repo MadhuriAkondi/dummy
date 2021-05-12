49f58e174195fb8fc2827a3d15b8ad1c58e7426e
ManojAkondi <63193358+mano3-1@users.noreply.github.com>
2021-05-12 10:30:33 +0530

    Update python.py

--- python.py
+++ python.py
@@ -11,12 +11,14 @@ def denomination(n):
         n=n%i
     if(n!=0):
         print("denomination not possible")
+        print("git test")
     else:
         for i in range(0,len(f)):
             print(l[i] , ":" , f[i])
             print("cash")
         
 n = int(input())
+print("git hello")
 denomination(n);
 print("Hi")
 print("Hello")
